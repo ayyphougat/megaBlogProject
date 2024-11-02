9fbbe8f2483260cf0152766421d5ca0b20605891
ayyphougat <phougat.ayush2@gmail.com>
2024-11-03 01:11:20 +0530

    added get started button on homepage.

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width = '100px',className = ''}) {
   return (
-    <img src="\public\blogVillaLogo.jpg" alt="logo" className={`h-10 w-10 ${className}`} />
+    <img src="blogVillaLogo.jpg" alt="logo" className={`h-10 w-10 ${className}`} />
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import appwriteService from "../appwrite/config";
-import { Container, PostCard } from '../components';
+import { Button, Container, PostCard } from '../components';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+
 
 function Home() {
     const [posts, setPosts] = useState([]);
@@ -44,13 +45,19 @@ function Home() {
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <div>
-                            <h1 className="mb-5 text-5xl font-normal font-rubik bg-clip-text text-transparent bg-gradient-to-t from-black to-red-800 leading-10">
+                            <h1 className="mb-5 text-5xl font-normal font-rubik bg-clip-text text-transparent bg-gradient-to-t from-slate-950 to-red-800 leading-10">
                                 Welcome to the Blog Villa !!
                             </h1>
-                            <p className='text-xl mt-10 font-mono font-bold p-3 flex justify-center capitalize'>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="52px" viewBox="0 -960 960 960" width="52px" fill="#000000" ><path d="m480-522 42 42 249-249-42-42-249 249ZM180-180h42l258-258-42-42-258 258v42Zm362-238L418-542l198-198-30-30-234 234-43-43 228-228q25-25 49.5-25.5T637-807l23 23 45-45q11-11 25-11t25 11l73 73q11 11 11 26t-11 26L542-418ZM244-120H120v-124l298-298 124 124-298 298Z"/></svg>
+                            <p className='text-xl mt-5 font-mono font-bold p-3 flex justify-center capitalize'>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="52px" viewBox="0 -960 960 960" width="52px" fill="#152C7C" ><path d="m480-522 42 42 249-249-42-42-249 249ZM180-180h42l258-258-42-42-258 258v42Zm362-238L418-542l198-198-30-30-234 234-43-43 228-228q25-25 49.5-25.5T637-807l23 23 45-45q11-11 25-11t25 11l73 73q11 11 11 26t-11 26L542-418ZM244-120H120v-124l298-298 124 124-298 298Z"/></svg>
                                 We Provide The Place <br /> You Provide The Words
+                                <svg xmlns="http://www.w3.org/2000/svg" height="52px" viewBox="0 -960 960 960" width="52px" fill="#152C7C" ><path d="m480-522 42 42 249-249-42-42-249 249ZM180-180h42l258-258-42-42-258 258v42Zm362-238L418-542l198-198-30-30-234 234-43-43 228-228q25-25 49.5-25.5T637-807l23 23 45-45q11-11 25-11t25 11l73 73q11 11 11 26t-11 26L542-418ZM244-120H120v-124l298-298 124 124-298 298Z"/></svg>
                             </p>
+                            <Button onClick={() => navigate("/all-posts")}
+                                className="bg-gradient-to-l from-slate-950 to-blue-800 rounded-3xl mt-3 transition duration-1000 ease-in-out hover:bg-gradient-to-r"
+                                >
+                                Get Started
+                            </Button>
                             </div>                    
                         </div>
                     </div>
